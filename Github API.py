7757c0deb2ba2f627decbba05fe3732a92b0c30d
Liu shengyuan <lius8@tcd.ie>
2019-11-05 20:00:34 +0000

    add the first function to realse the login in

--- Github API.py
+++ Github API.py
@@ -1,31 +1,53 @@
 import requests
 import json
 from github import Github
+import getpass
 
-url = 'https://api.github.com/repos/sebastianbergmann/php-code-coverage'
+def start():
+        access = False
+        while (not access):
+            username = input("Please put in your username: ")
+            password = getpass.getpass("Please input your password: ")
+            try:
+                g = Github(username, password)
+                access = True
+            except:
+                print("Incorrect details try again")
 
-
-print("Status:",requests.get(url).status_code)
-
-
-def getTheContributors():
-    curl='https://api.github.com/repos/sebastianbergmann/php-code-coverage/contributors'
-    r=requests.get(curl)
-
-    count=0
-    while count<len(r.json()):
-         theContributors=r.json()[count]['login']
-         print(theContributors)
-         count+=1
-    return theContributors
-
-getTheContributors()
-
-def getTheCommits():
-    curl = 'https://api.github.com/repos/sebastianbergmann/php-code-coverage/commits'
-    r=requests.get(curl)
-
+        error = True
+        while (error):
+            try:
+                repo_to_use = input("Please put in the git repo you wish to interrogate: ")
+                repo = g.get_repo(repo_to_use)
+                error = False
+            except:
+                print("Incorrect repo please try again")
+        return repo
 
 
 
+def getTheContributors(repo):
+    repo_contributors = []
+    contributors = repo.get_contributors()
+    print("Preparing to print the contributors and each of their contributions:")
+    for contributor in contributors:
+            name = str(contributor.name)
+            contributionTime = str(contributor.contributions)
+            if name != "None":
+                if contributionTime != "None":
+                   print("Contributor:" + str(name)+"  Contribution:"+ str(contributionTime))
+                   repo_contributors.append(contributor)
+                   repo_contributors.append(int(contributionTime))
+    # returns contributors in a list
+
+    return repo_contributors
+
+
+
+
+
+
+repo=start()
+contributors = getTheContributors(repo)
+print(contributors)
 
