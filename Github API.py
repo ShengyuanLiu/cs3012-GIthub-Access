c818ee7f54eb7999563e58cd1830d67d1b24c57c
Liu shengyuan <lius8@tcd.ie>
2019-11-13 21:56:39 +0000

    working 3 hours and relase the function to get the contributors' repo amounts

--- Github API.py
+++ Github API.py
@@ -31,29 +31,53 @@ def start():
 
 
 def getTheContributors(repo):
+    owner = repo.owner
     repo_contributors = []
     repo_contributions = []
     contributors = repo.get_contributors()
     print("Preparing to print the contributors and each of their contributions:")
     for contributor in contributors:
-            name = str(contributor.name)
-            contributionTime = str(contributor.contributions)
-            if name != "None":
+            if(contributor!=owner):
+              name = str(contributor.name)
+              contributionTime = str(contributor.contributions)
+              if name != "None":
                 if contributionTime != "None":
-                   print("Contributor:" + str(name)+"  Contribution:"+ str(contributionTime))
+                   # print("Contributor:" + str(name)+"  Contribution:"+ str(contributionTime))
                    repo_contributors.append(contributor)
                    repo_contributions.append(contributionTime)
-    # returns contributors in a list
+    # returns contributors in a list and his/her contributions in another list
 
     return repo_contributors,repo_contributions
 
-# def getTheFollower():
-#
+def getTheContributorsRepo(contributors):
+    repo_count=[]
+    count=0
+    print("Prepare to print the repo number:")
+    for index in contributors:
+       repoNumber = index.get_repos()
+       repoNumbers=len(list(repoNumber))
+       # print(repoNumbers)
+       if repoNumbers != "None":
+          print("His/her repository's amount is " +str(repoNumbers))
+          repo_count.append(str(repoNumbers))
+
+
+    return repo_count
+
 
 
 
 
 repo=start()
 (contributors,contributions) = getTheContributors(repo)
+# print the greatest contributors and contributions
 print("\nThe contributors who gives the most contributions is " +contributors[0].name+" and his/her contribution time is:"+contributions[0])
+repo_count = getTheContributorsRepo(contributors)
+
+# to print each contributor's repository amount
+# count=0
+# while count<len(repo_count):
+#   theRepo=repo_count[count]
+#   print("Each contributor's repository amount is "+str(theRepo))
+#   count+=1
 
