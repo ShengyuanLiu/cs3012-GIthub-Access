9e1726ef03337924d59dec14b4e6d4bc1cfcd125
Liu shengyuan <lius8@tcd.ie>
2019-11-15 15:00:51 +0000

    add the count watch events function

--- Github API.py
+++ Github API.py
@@ -67,14 +67,14 @@ def getTheContributorsRepo(contributors):
 
 
 
-def getReceivedEvents(contributors):
+def getReceivedIssueEvents(contributors):
     received_count=[]
     issue_count=[]
-    print("Prepare to print the recevied event amounts")
+    print("Prepare to print the recevied issue event amounts")
     for index in contributors:
         recevied = index.get_received_events()
         recevieds=len(list(recevied))
-        print(str(recevieds))
+        # print(str(recevieds))
         received_count.append(recevieds)
         push_event=list(index.get_received_events())
         push_event=str(push_event)
@@ -86,17 +86,48 @@ def getReceivedEvents(contributors):
               pattern=re.compile(r'IssuesEvent')
               result=pattern.findall(push_event_str)
               theIssue=len(result)
-              print(theIssue)
+              # print(theIssue)
               issue_count.append(str(theIssue))
 
         elif push_event==[]:
-               print(push_event_str)
+               # print(push_event_str)
                theIssue=0
-               print(theIssue)
+               # print(theIssue)
                issue_count.append(str(theIssue))
 
 
     return received_count,issue_count
+
+def getReceivedWatchEvents(contributors):
+    received_count=[]
+    watch_count=[]
+    print("Prepare to print the recevied watch event amounts")
+    for index in contributors:
+        recevied = index.get_received_events()
+        recevieds=len(list(recevied))
+        # print(str(recevieds))
+        received_count.append(recevieds)
+        push_event=list(index.get_received_events())
+        push_event=str(push_event)
+        # print(push_event)
+        theIssue=0
+        if push_event!=[]:
+              push_event_str="".join(push_event)
+              # print(push_event_str)
+              pattern=re.compile(r'WatchEvent')
+              result=pattern.findall(push_event_str)
+              theIssue=len(result)
+              # print(theIssue)
+              watch_count.append(str(theIssue))
+
+        elif push_event==[]:
+               # print(push_event_str)
+               theIssue=0
+               # print(theIssue)
+               watch_count.append(str(theIssue))
+
+
+    return received_count,watch_count
 
 
 def getStars(contributors):
@@ -106,7 +137,7 @@ def getStars(contributors):
         star=index.get_starred()
         starAmounts =len(list(star))
         print(starAmounts)
-        orgName=list(star)
+        # orgName=list(star)
         # print(orgName)
         stars.append(str(starAmounts))
 
@@ -114,9 +145,15 @@ def getStars(contributors):
     return stars
 
 
+def getCommits(repo):
+    commits=[]
+    print("Prepare to print the repo commits")
+    theCommits=repo.get_commits()
+    listCommits=list(theCommits)
+    theLen=len(listCommits)
+    print(listCommits)
 
-
-
+    return commits
 
 
 
@@ -124,31 +161,47 @@ repo=start()
 (contributors,contributions) = getTheContributors(repo)
 # print the greatest contributors and contributions
 print("\nThe contributors who gives the most contributions is " +contributors[0].name+" and his/her contribution time is:"+contributions[0])
-# repo_count = getTheContributorsRepo(contributors)
-(received_count,issue_count) = getReceivedEvents(contributors)
-# starred_count=getStars(contributors)
+repo_count = getTheContributorsRepo(contributors)
+(received1_count,issue_count) = getReceivedIssueEvents(contributors)
+starred_count=getStars(contributors)
+(received2_count,watch_count)=getReceivedWatchEvents(contributors)
+# commits_count=getCommits(repo)
+
 # to print each contributor's repository amount
-# count=0
-# while count<len(repo_count):
-#   theRepo=repo_count[count]
-#   print("Each contributor's repository amount is "+str(theRepo))
-#   count+=1
+count1=0
+while count1<len(repo_count):
+  theRepo=repo_count[count1]
+  print("Each contributor's repository amount is "+str(theRepo))
+  count1+=1
+
 
 #get the contributors' code type
 # if the result>2 means the user prefer to participate in other people's code development
 # if the result<1 means the user prefer to develop his/her own code
-# count=0
-# while count<len(repo_count):
-#     result=int(starred_count[count])/int(repo_count[count])
-#     print("The result are: "+str(result))
-#     count+=1
+count2=0
+while count2<len(repo_count):
+    result=int(starred_count[count2])/int(repo_count[count2])
+    print("The result are: "+str(result))
+    count2+=1
 
-#get the contributors' work quality
+
+#get the contributors' work quality within the last 90 days
 # if result>0.2 means the contributor's work quality is not very good
 # if 0<result<0.05 means the contributor has a great code ability
 # if result=0 should check the user's event excalty
-# count=0
-# while count<len(received_count):
-#       result=int(received_count[count])/int(issue_count[count])
-#       print("The contributors' work quality result is: "+str(result))
-#       count+=1
+count3=0
+while count3<len(received1_count):
+      result=int(issue_count[count3])/int(received1_count[count3])
+      print("The contributors' work quality result is: "+str(result))
+      count3+=1
+
+
+#get the contributors' work quality within the last 90 days
+# if result>0.2 means the contributor's work quality is not very good
+# if 0<result<0.05 means the contributor has a great code ability
+# if result=0 should check the user's event excalty
+count4=0
+while count4<len(received1_count):
+      result=int(watch_count[count4])/int(received2_count[count4])
+      print("The contributors' repo authority result is: "+str(result))
+      count4+=1
